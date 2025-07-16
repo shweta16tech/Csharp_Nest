ec16ca7123dec5fd42d89b84b880aecdbb78e09b
Shweta <shwetan_techsys06@gmail.com>
2025-07-16 12:50:47 +0545

    Reapply .gitignore rules to remove unnecessary tracked files

---
+++ OrangeStore/OrangeStore/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrangeStore.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ OrangeStore/OrangeStore/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace OrangeStore.Models
+{
+    public class Product
+    {
+        public int Id;
+        public string Name;
+        public string Description;
+        public int Price;
+    }
+}
